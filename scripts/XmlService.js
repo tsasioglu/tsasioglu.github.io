4553cbfd2b7ccb9bc47cc1a8aefdbcf2bc6efcc0
Taran <tsasioglu@users.noreply.github.com>
2019-11-07 16:45:26 +0000

    Not json

--- scripts/XmlService.js
+++ scripts/XmlService.js
@@ -5,7 +5,7 @@
             //var yql = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent("select * from xml where url='" + url + "'") + "&format=xml&callback=?";
             var deferred = $q.defer();
 
-            $.getJSON(url, function (data)
+            $.get(url, function (data)
             {
 				console.log(data)
                 var xml = $(data);
