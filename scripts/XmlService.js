932a87c1af073236241c1279f0708036e32896ea
Taran <tsasioglu@users.noreply.github.com>
2019-11-07 15:07:14 +0000

    Make yahoo API call https

--- scripts/XmlService.js
+++ scripts/XmlService.js
@@ -2,7 +2,7 @@
     .factory("XmlService", ["$q", function($q) {
         
         var downloadXml = function(url) {
-            var yql = "http://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent("select * from xml where url='" + url + "'") + "&format=xml&callback=?";
+            var yql = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent("select * from xml where url='" + url + "'") + "&format=xml&callback=?";
             var deferred = $q.defer();
 
             $.getJSON(yql, function (data)
